da8663e670a49eaa8ed16c7ee0643c2107ad57ec
Parth-1603 <arnavparth132@gmail.com>
2026-04-26 23:54:55 +0530

    leaderboard

--- app/api/leaderboard/route.ts
+++ app/api/leaderboard/route.ts
@@ -1,8 +1,10 @@
 import { NextResponse } from "next/server";
 import { getLeaderboard } from "@/lib/db";
 
-export const dynamic = 'force-dynamic';
+
+export const dynamic = "force-dynamic";
 export const revalidate = 0;
+
 
 export async function GET() {
     try {
